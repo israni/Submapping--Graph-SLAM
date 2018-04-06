d1caa1ad1222cecf44bc86a42839337eb3939423
eeyan <eeyan@umich.edu>
2018-04-06 18:03:26 -0400

    optimizes submaps

--- create_submaps.m
+++ create_submaps.m
@@ -10,13 +10,19 @@ num_submaps = 4;
 num_points_total = 810; % total # of points in dataset
 num_points_submap = ceil(num_points_total / num_submaps);
 
-%% Create graph containers and add factors to it
+%% Create graph containers
 graph(num_submaps) = NonlinearFactorGraph;
 initial_estimate(num_submaps) = Values;
-prior_pose = -1 * ones(1,4);
+for i = 1:num_submaps-1 % last one is already initialized
+    graph(i) = NonlinearFactorGraph;
+    initial_estimate(i) = Values;
+end
+prior_pose = -1 * ones(3,num_submaps);
+prior_vertex = -1 * ones(1,num_submaps);
+% noise = zeros(3);
 
+%% Parse data and add factors
 while ischar(input_line)
-    %disp(input_line)
     split_line = strsplit(input_line);
     
     %% ADD VERTICES TO GRAPH - INITIAL ESTIMATE %%
@@ -31,8 +37,11 @@ while ischar(input_line)
             submap_idx = floor(vertex_id / num_points_submap) + 1;
             initial_estimate(submap_idx).insert(vertex_id, Pose2(x,y,theta));
             
-            if (prior_pose(submap_idx) == -1)
-                prior_pose(submap_idx) = vertex_id;
+            if (prior_pose(1,submap_idx) == -1)
+                prior_pose(1,submap_idx) = x;
+                prior_pose(2,submap_idx) = y;
+                prior_pose(3,submap_idx) = theta;
+                prior_vertex(submap_idx) = vertex_id;
             end
         end
     end
@@ -66,8 +75,7 @@ while ischar(input_line)
             submap_idx_1 = floor(vertex_id_1 / num_points_submap) + 1;
             submap_idx_2 = floor(vertex_id_2 / num_points_submap) + 1;
             if (submap_idx_1 == submap_idx_2) % non-boundary node
-                graph(submap_idx_1).add(BetweenFactorPose2(...
-                    vertex_id_1, vertex_id_2, Pose2(dx, dy, dtheta), model));
+                graph(submap_idx_1).add(BetweenFactorPose2(vertex_id_1, vertex_id_2, Pose2(dx, dy, dtheta), model));
             else % boundary node
                 % do stuff
             end          
@@ -80,3 +88,32 @@ end
 
 fclose(data_file);
 
+%% Add priors
+prior_noise = noiseModel.Diagonal.Sigmas([0.3; 0.3; 0.1]);
+for i = 1:num_submaps
+%     for j = 1:num_submaps
+%         graph(j).print(sprintf('\nFactor graph:\n'));
+%     end
+    graph(i).add(PriorFactorPose2(prior_vertex(i), Pose2(prior_pose(1,i), prior_pose(2,i), prior_pose(3,i)), prior_noise));
+%     for j = 1:num_submaps
+%         graph(j).print(sprintf('\nFactor graph:\n'));
+%     end
+end
+
+%% Optimize and plot results
+
+parameters = DoglegParams();
+
+parameters.setRelativeErrorTol(1e-5);
+parameters.setMaxIterations(1000);
+
+for i = 1:num_submaps
+    optimizer = gtsam.DoglegOptimizer(graph(i), initial_estimate(i), parameters);
+    result(i) = optimizer.optimizeSafely();
+    
+    figure(i);
+    plot2DTrajectory(result(i));
+    axis equal
+    axis tight
+    view(2)
+end
