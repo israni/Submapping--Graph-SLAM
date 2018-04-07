4473618f130c68e19a3667ac83dd7eaa758d605d
eeyan <eeyan@umich.edu>
2018-04-06 22:00:51 -0400

    added separator optimizer

--- create_submaps.m
+++ create_submaps.m
@@ -3,33 +3,74 @@ close all;
 
 import gtsam.*
 
-data_file = fopen('input_INTEL_g2o.g2o');
-input_line = fgetl(data_file);
-
+%% Set these variables
 num_submaps = 4;
 num_points_total = 810; % total # of points in dataset
 num_points_submap = ceil(num_points_total / num_submaps);
+separator_nodes = zeros(1,num_points_total);
 
 %% Create graph containers
 graph(num_submaps) = NonlinearFactorGraph;
 initial_estimate(num_submaps) = Values;
-for i = 1:num_submaps-1 % last one is already initialized
+for i = 1:num_submaps
     graph(i) = NonlinearFactorGraph;
     initial_estimate(i) = Values;
+    separator_nodes(num_points_submap*(i-1)+1) = 1;
 end
-prior_pose = -1 * ones(3,num_submaps);
-prior_vertex = -1 * ones(1,num_submaps);
+separator_graph = NonlinearFactorGraph;
+separator_estimate = Values;
+base_pose = -1 * ones(3,num_submaps);
+base_vertex = -1 * ones(1,num_submaps);
 % noise = zeros(3);
 
 %% Parse data and add factors
+data_file = fopen('input_INTEL_g2o.g2o');
+input_line = fgetl(data_file);
+
 while ischar(input_line)
     split_line = strsplit(input_line);
     
+    %% ADD EDGES TO GRAPH - ODOMETRY AND POSE CONTRAINTS %%
+    if (strcmp(split_line{1},'EDGE_SE2'))
+        vertex_id_1 = str2double(split_line{2});
+        vertex_id_2 = str2double(split_line{3});
+        if (vertex_id_1 < num_points_total && vertex_id_2 < num_points_total)
+            odometry = ([str2double(split_line{4}),str2double(split_line{5}),str2double(split_line{6})]);
+            dx = odometry(1);
+            dy = odometry(2);
+            dtheta = odometry(3);
+
+            information_matrix(1,1)= str2double(split_line{7});
+            information_matrix(1,2)= str2double(split_line{8});
+            information_matrix(1,3)= str2double(split_line{9});
+            information_matrix(2,1)= str2double(split_line{8});
+            information_matrix(2,2)= str2double(split_line{10});
+            information_matrix(2,3)= str2double(split_line{11});
+            information_matrix(3,1)= str2double(split_line{9});
+            information_matrix(3,2)= str2double(split_line{11});
+            information_matrix(3,3)= str2double(split_line{12});
+
+            chol_information_matrix = sqrtm(information_matrix);
+            model = noiseModel.Gaussian.SqrtInformation(chol_information_matrix);
+            
+            submap_idx_1 = floor(vertex_id_1 / num_points_submap) + 1;
+            submap_idx_2 = floor(vertex_id_2 / num_points_submap) + 1;
+            if (submap_idx_1 == submap_idx_2) % non-boundary node
+                graph(submap_idx_1).add(BetweenFactorPose2(vertex_id_1, vertex_id_2, Pose2(dx, dy, dtheta), model));
+            else % boundary node
+                % keep track of which nodes are used for separator optimization
+                separator_nodes(vertex_id_1+1) = 1;
+                separator_nodes(vertex_id_2+1) = 1;
+                separator_graph.add(BetweenFactorPose2(vertex_id_1, vertex_id_2, Pose2(dx, dy, dtheta), model));
+            end          
+        end
+    end
+    
     %% ADD VERTICES TO GRAPH - INITIAL ESTIMATE %%
     if (strcmp(split_line{1},'VERTEX_SE2'))
-        vertex_id = str2num(split_line{2});
-        if (vertex_id <= num_points_total)
-            vertex_location = ([str2num(split_line{3}),str2num(split_line{4}),str2num(split_line{5})]);
+        vertex_id = str2double(split_line{2});
+        if (vertex_id < num_points_total)
+            vertex_location = ([str2double(split_line{3}),str2double(split_line{4}),str2double(split_line{5})]);
             x = vertex_location(1);
             y = vertex_location(2);
             theta = vertex_location(3);
@@ -37,48 +78,20 @@ while ischar(input_line)
             submap_idx = floor(vertex_id / num_points_submap) + 1;
             initial_estimate(submap_idx).insert(vertex_id, Pose2(x,y,theta));
             
-            if (prior_pose(1,submap_idx) == -1)
-                prior_pose(1,submap_idx) = x;
-                prior_pose(2,submap_idx) = y;
-                prior_pose(3,submap_idx) = theta;
-                prior_vertex(submap_idx) = vertex_id;
+            % add base nodes to use as priors
+            if (base_pose(1,submap_idx) == -1)
+                base_pose(1,submap_idx) = x;
+                base_pose(2,submap_idx) = y;
+                base_pose(3,submap_idx) = theta;
+                base_vertex(submap_idx) = vertex_id;
+%                 separator_estimate.insert(vertex_id, Pose2(x,y,theta));
             end
-        end
-    end
-    
-    %% ADD EDGES TO GRAPH - ODOMETRY AND POSE CONTRAINTS %%
-    if (strcmp(split_line{1},'EDGE_SE2'))
-        vertex_id_1 = str2num(split_line{2});
-        vertex_id_2 = str2num(split_line{3});
-        if (vertex_id_1 <= num_points_total && vertex_id_2 <= num_points_total)
-            odometry = ([str2num(split_line{4}),str2num(split_line{5}),str2num(split_line{6})]);
-            dx = odometry(1);
-            dy = odometry(2);
-            dtheta = odometry(3);
-
-            information_matrix(1,1)= str2num(split_line{7});
-            information_matrix(1,2)= str2num(split_line{8});
-            information_matrix(1,3)= str2num(split_line{9});
-            information_matrix(2,1)= str2num(split_line{8});
-            information_matrix(2,2)= str2num(split_line{10});
-            information_matrix(2,3)= str2num(split_line{11});
-            information_matrix(3,1)= str2num(split_line{9});
-            information_matrix(3,2)= str2num(split_line{11});
-            information_matrix(3,3)= str2num(split_line{12});
-
-            chol_information_matrix = sqrtm(information_matrix);
-            model = noiseModel.Gaussian.SqrtInformation(chol_information_matrix);
-%             if (noise(1,1) == 0)
-%                 noise = chol_information_matrix;
-%             end
             
-            submap_idx_1 = floor(vertex_id_1 / num_points_submap) + 1;
-            submap_idx_2 = floor(vertex_id_2 / num_points_submap) + 1;
-            if (submap_idx_1 == submap_idx_2) % non-boundary node
-                graph(submap_idx_1).add(BetweenFactorPose2(vertex_id_1, vertex_id_2, Pose2(dx, dy, dtheta), model));
-            else % boundary node
-                % do stuff
-            end          
+            % add inter-measurement nodes but don't double count base nodes
+%             if (separator_nodes(vertex_id+1) == 1 && base_vertex(submap_idx) ~= vertex_id)
+            if (separator_nodes(vertex_id+1) == 1)
+                separator_estimate.insert(vertex_id, Pose2(x,y,theta));
+            end
         end
     end
     
@@ -94,11 +107,12 @@ for i = 1:num_submaps
 %     for j = 1:num_submaps
 %         graph(j).print(sprintf('\nFactor graph:\n'));
 %     end
-    graph(i).add(PriorFactorPose2(prior_vertex(i), Pose2(prior_pose(1,i), prior_pose(2,i), prior_pose(3,i)), prior_noise));
+    graph(i).add(PriorFactorPose2(base_vertex(i), Pose2(base_pose(1,i), base_pose(2,i), base_pose(3,i)), prior_noise));
 %     for j = 1:num_submaps
 %         graph(j).print(sprintf('\nFactor graph:\n'));
 %     end
 end
+separator_graph.add(PriorFactorPose2(base_vertex(1), Pose2(base_pose(1,1), base_pose(2,1), base_pose(3,1)), prior_noise));
 
 %% Optimize and plot results
 
@@ -107,13 +121,25 @@ parameters = DoglegParams();
 parameters.setRelativeErrorTol(1e-5);
 parameters.setMaxIterations(1000);
 
-for i = 1:num_submaps
-    optimizer = gtsam.DoglegOptimizer(graph(i), initial_estimate(i), parameters);
-    result(i) = optimizer.optimizeSafely();
+% for i = 1:num_submaps
+%     optimizer = gtsam.DoglegOptimizer(graph(i), initial_estimate(i), parameters);
+%     result = optimizer.optimizeSafely();
     
-    figure(i);
-    plot2DTrajectory(result(i));
-    axis equal
-    axis tight
-    view(2)
-end
+%     figure(i);
+%     plot2DTrajectory(result);
+%     axis equal
+%     axis tight
+%     view(2)
+% end
+
+separator_graph.print(sprintf('\nFactor graph:\n'));
+parameters = LevenbergMarquardtParams();
+optimizer = gtsam.LevenbergMarquardtOptimizer(separator_graph, separator_estimate, parameters);
+result = optimizer.optimizeSafely();
+result.print(sprintf('\nFinal result:\n'));
+
+figure(1);
+plot2DTrajectory(result);
+axis equal
+axis tight
+view(2)
